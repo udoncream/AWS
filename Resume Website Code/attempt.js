6c5b852fc4a6860eae562540bcaba43d4e7ce1da
Alberto Estrada <122493968+udoncream@users.noreply.github.com>
2024-04-18 18:00:35 -0400

    Add files via upload

---
+++ Resume Website Code/attempt.js
@@ -0,0 +1,8 @@
+fetch ('https://sbf6r47lld.execute-api.us-west-1.amazonaws.com/Test/')  
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        const guestCount= document.getElementById('visitorCount');
+        guestCount.innerHTML = data
+    });
