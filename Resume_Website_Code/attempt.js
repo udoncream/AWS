875a3e3f634d83ad6937b00ba49996945dcbe4bc
Alberto Estrada <122493968+udoncream@users.noreply.github.com>
2024-04-22 16:03:58 -0400

    Update attempt.js

--- Resume_Website_Code/attempt.js
+++ Resume_Website_Code/attempt.js
@@ -6,3 +6,4 @@ fetch ('https://sbf6r47lld.execute-api.us-west-1.amazonaws.com/Test/')
         const guestCount= document.getElementById('visitorCount');
         guestCount.innerHTML = data
     });
+//test
